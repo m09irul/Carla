f88e2165d1a474887f3913712cc2048d9e24a9b1
Monirul Islam <shanto4045@gmail.com>
2024-02-27 23:14:00 +0900

    stt and tts done

---
+++ stt_tts_test.py
@@ -0,0 +1,56 @@
+# Python program to translate
+# speech to text and text to speech
+
+
+import speech_recognition as sr
+import pyttsx3 
+
+# Initialize the recognizer 
+r = sr.Recognizer() 
+
+# Function to convert text to
+# speech
+def SpeakText(command):
+	# Initialize the engine
+	engine = pyttsx3.init()
+	voice_id = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-GB_HAZEL_11.0'
+	engine.setProperty('voice', voice_id)
+		
+	engine.say(command) 
+	engine.runAndWait()
+	
+    
+	
+	
+# Loop infinitely for user to
+# speak
+
+while(1): 
+	
+	# Exception handling to handle
+	# exceptions at the runtime
+	try:
+		
+		# use the microphone as source for input.
+		with sr.Microphone(2) as source2:
+			
+			# wait for a second to let the recognizer
+			# adjust the energy threshold based on
+			# the surrounding noise level 
+			r.adjust_for_ambient_noise(source2, duration=0.2)
+			
+			#listens for the user's input 
+			audio2 = r.listen(source2)
+			
+			# Using google to recognize audio
+			MyText = r.recognize_google(audio2)
+			MyText = MyText.lower()
+
+			print("Did you say ",MyText)
+			SpeakText(MyText)
+			
+	except sr.RequestError as e:
+		print("Could not request results; {0}".format(e))
+		
+	except sr.UnknownValueError:
+		print("unknown error occurred")
